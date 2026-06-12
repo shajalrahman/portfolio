b45d6b40b9f000e519e0ada08753c143841eb296
Shajal Rahman <shajalr313@gmail.com>
2026-06-12 17:12:20 +0530

    Updates

--- assets/Navbar-BLv0p5Uw.js
+++ assets/Navbar-BLv0p5Uw.js
@@ -1,4 +1,4 @@
-import{j as P,r as bt}from"./index-D6gcH6Ga.js";import{g as St,S as yt}from"./ScrollTrigger-CiEuWA-R.js";/*!
+import{j as P,r as bt}from"./index-fTyGO71M.js";import{g as St,S as yt}from"./ScrollTrigger-CiEuWA-R.js";/*!
  * SplitText 3.15.0
  * https://gsap.com
  *
